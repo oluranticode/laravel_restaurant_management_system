2da818713a0dcd91a828e91d7b1a493306e269c7
youngtemmy <alukotemitope49@gmail.com>
2022-04-14 12:55:57 +0100

    updated

--- resources/views/admin/admin-footer.blade.php
+++ resources/views/admin/admin-footer.blade.php
@@ -3,7 +3,7 @@
           <footer class="footer">
             <div class="d-sm-flex justify-content-center justify-content-sm-between">
               <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © temzycode</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">TemzyCode</a> from Temitope Aluko</span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> <a href="https://temitope-portfolio.netlify.app/" target="_blank">Potfolio</a> TemzyCode</span>
             </div>
           </footer>
           <!-- partial -->
